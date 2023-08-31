a794393aff95331be68b761558f16aa7e0380deb
dodam24 <dodam0724@gmail.com>
2023-09-01 00:29:43 +0900

    TSC, 타입스크립트 설정 파일, 기본 타입

---
+++ learn-typescript/playground/playground.js
@@ -0,0 +1,9 @@
+function sum(a, b) {
+    return a + b;
+}
+var Student = /** @class */ (function () {
+    function Student(name) {
+        this.name = name;
+    }
+    return Student;
+}());
---
+++ learn-typescript/playground/playground.ts
@@ -0,0 +1,9 @@
+function sum(a: number, b: number) {
+    return a + b;
+}
+
+class Student {
+    constructor(name: string) {
+        this.name = name;
+    }
+}
