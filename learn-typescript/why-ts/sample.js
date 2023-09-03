c13f86f56ac46b08bf5c3f7fc2c8145ebcc02e93
dodam24 <dodam0724@gmail.com>
2023-09-03 19:07:36 +0900

    why-ts, BNF

---
+++ learn-typescript/why-ts/sample.js
@@ -0,0 +1,18 @@
+// function sum(a, b) {
+//     return a + b;
+// }
+// // sum(10, '20');  // 1020
+// var result = sum(10, 20);
+// result.toLocaleString();
+
+// @ts-check    // JavaScript 파일에서 타입 체크를 수행
+
+/**
+ * 
+ * @param {number} a 첫번째 숫자
+ * @param {number} b 두번째 숫자
+ */
+function sum(a, b) {
+    return a + b;
+}
+sum(10, '20');
---
+++ learn-typescript/why-ts/sample.ts
@@ -0,0 +1,8 @@
+function add(a: number, b: number): number {
+    return a + b;
+}
+
+var result = add(10, 20);
+result.toLocaleString(); // toLocaleString(): 숫자를 문자열로 변환해주는 함수
+
+// add(10, '20');
