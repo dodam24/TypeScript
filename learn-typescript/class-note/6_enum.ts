8d86ece9f263459127dac12cf3a0e339803f8e53
dodam24 <dodam0724@gmail.com>
2023-10-04 16:11:40 +0900

    [타입스크립트 입문] 이넘(Enums)

---
+++ learn-typescript/class-note/6_enum.ts
@@ -0,0 +1,40 @@
+// 이넘(Enums): 특정 값들의 집합을 의미하는 자료형
+
+// 숫자형 이넘
+// enum Shoes {
+//   Nike,
+//   Adidas
+// }
+
+// var myShoes = Shoes.Nike;
+// console.log(myShoes); // 0
+
+
+// 문자형 이넘
+enum Shoes {
+  Nike = '나이키',
+  Adidas = '아디다스'
+}
+
+var myShoes = Shoes.Nike;
+console.log(myShoes); // '나이키'
+
+
+// 예제
+enum Answer {
+  Yes = 'Y',
+  No = 'N',
+}
+function askQuestion(answer: Answer) {
+  if (answer === Answer.Yes) {
+    console.log('정답입니다');
+  }
+  if (answer === Answer.No) {
+    console.log('오답입니다');
+  }
+}
+askQuestion(Answer.Yes);
+// askQuestion('Yes');
+// askQuestion('예스');
+// askQuestion('y');
+// askQuestion('yes');
